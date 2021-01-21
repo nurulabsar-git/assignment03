072f98b67b8d7ee88137d6ca8a1cbccdad570e91
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-01-22 01:36:26 +0600

    complete

--- assignment.js
+++ assignment.js
@@ -1,33 +1,29 @@
-
+// https://github.com/nurulabsar-git/assignment03
 
 //Answer Number one.
 function kilometerToMeter(kilometer){
     let meter = kilometer*1000;
     return meter;
 }
-
-var convertMeter = kilometerToMeter(10);
-console.log(convertMeter);
+var convertMeter = kilometerToMeter(100);
+console.log(convertMeter, "meter");
 
 
 //Answer Number Two.
 function budgetCalculator(watch, phone, laptop){
 
     if(watch<=0 || phone<=0 || laptop<=0){
-        return "wrong number";
+        return -1;
     }
 
     else{
-    var watchPrice = watch*50;
-    var phonePrice = phone*100;
-    var laptopPrice = laptop*500;
+    var watchPrice = watch*4200;  //[here 1$ = 84 tk]
+    var phonePrice = phone*8400;
+    var laptopPrice = laptop*42000;
     var totalPrice = watchPrice + phonePrice + laptopPrice;
     return totalPrice;
-
     }
-
 }
-
 var result = budgetCalculator(2, 5, 4);
 console.log(result);
 
@@ -40,30 +36,28 @@ function hotelCost(day){
       cost = day*100;
   }
   else if(day<=20){
-      var tenDayCost = day*100;
+      var tenDaysCost = day*100;
       var remaining = day - 10;
-      var afterTenDay = remaining*80;
-      cost = tenDayCost + afterTenDay;
+      var afterTenDays = remaining*80;
+      cost = tenDaysCost + afterTenDays;
   }
  
   else{
-      var tenDayCost = day*100;
-      var afterTenDay = 10*80;
+      var tenDaysCost = day*100;
+      var afterTenDays = 10*80;
       var remaining = day - 20;
-      var afterTweenty = remaining*50;
-      cost = tenDayCost + afterTenDay + afterTweenty;
+      var afterTwenty = remaining*50;
+      cost = tenDaysCost + afterTenDays + afterTwenty;
   }
  return cost;
-
 }
-
 var totalCost = hotelCost(22);
-console.log(totalCost);
+console.log(totalCost, "Taka");
 
 
 
 // Answer Number four.
-megaFriend = ["nurulabsar", "absar", "rifaat"];
+megaFriend = ["Nurul Absar", "Absar", "Rifat", "Arafat"];
 var lengthiFriend = megaFriend[0];
 
 for(var i = 0; i>megaFriend.length; i++){
@@ -75,4 +69,15 @@ for(var i = 0; i>megaFriend.length; i++){
 console.log(lengthiFriend);
 
 
-/*   Thank You  */
+
+
+/*Extra problem solve
+ Question: convert meter into kilometer?
+*/
+
+function meterToKilometer(meter){
+    var kilometer = meter/1000;
+    return kilometer;
+}
+var convertKiloMeter = meterToKilometer(10000);
+console.log(convertKiloMeter, "km");
